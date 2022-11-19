3f2c4878242532b839e99adb58f4a4c6f0780992
Mary Snegova <Marya0@yandex.ru>
2022-11-19 14:39:33 +0300

    feat: add CORS

--- app.js
+++ app.js
@@ -10,12 +10,29 @@ const { limiter } = require('./middlewares/rateLimiter');
 
 const error = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { allowedCors } = require('./constants/constants');
 
 const { NODE_ENV, URL_DB, PORT = 3000 } = process.env;
 
 const app = express();
 
 mongoose.connect(NODE_ENV === 'production' ? URL_DB : 'mongodb://localhost:27017/moviesdb');
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- constants/constants.js
+++ constants/constants.js
@@ -1,7 +1,14 @@
 const SALT_OR_ROUNDS = 10;
 const regex = /(https?:\/\/)([www.]?[a-zA-Z0-9-]+\.)([^\s]{2,})/;
 
+const allowedCors = [
+  'https://movies-explr.msnegova.nomoredomains.icu',
+  'http://movies-explr.msnegova.nomoredomains.icu',
+  'http://localhost:3000',
+];
+
 module.exports = {
   SALT_OR_ROUNDS,
   regex,
+  allowedCors,
 };
